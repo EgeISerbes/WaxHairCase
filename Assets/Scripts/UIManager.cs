e8373a5788ea6a42736b555ea284698b81feae14
egeserbes <egeserbes55@gmail.com>
2022-09-05 22:15:52 +0200

    added ui

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UIManager : MonoBehaviour
+{
+    private System.Action<bool> _onGameStart;
+    [SerializeField] private GameObject _playButton;
+    [SerializeField] private GameObject _pauseMenu;
+    [SerializeField] private GameObject _pauseButton;
+    [SerializeField] private float _waitSeconds; 
+    public void Init(System.Action<bool> GameStarted)
+    {
+        _onGameStart = GameStarted;
+        _playButton.SetActive(true);
+        _pauseButton.SetActive(false);
+        _pauseMenu.SetActive(false);
+    }
+
+    public void StartPlaying()
+    {
+        _playButton.SetActive(false);
+        GameResumed();
+    }
+    public void GamePaused()
+    {
+        _pauseButton.SetActive(false);
+        _onGameStart(false);
+    }
+
+    public void GameResumed()
+    {
+        _pauseButton.SetActive(false);
+        _onGameStart(true);
+    }
+    public void GameRestarted()
+    {
+        StartCoroutine(RestartGame());
+    }
+    IEnumerator RestartGame()
+    {
+        yield return new WaitForSeconds(_waitSeconds);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+}
